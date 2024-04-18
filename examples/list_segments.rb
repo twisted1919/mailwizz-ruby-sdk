2c13f1f42913d3d90e85e424f0083dfb40985dc2
ghimes <ghimescosmin@gmail.com>
2024-04-18 10:54:48 +0300

    added new endpoint that gets the subscribers of a segment

--- examples/list_segments.rb
+++ examples/list_segments.rb
@@ -4,8 +4,20 @@ require '../examples/setup_api'
 # CREATE THE ENDPOINT
 endpoint = ListSegments.new
 
-# GET ALL FIELDS OF A LIST
+# GET ALL SEGMENTS OF A LIST
 response = endpoint.get_segments('LIST_UID')
+
+# DISPLAY RESPONSE
+puts response.body
+
+# GET ONE LIST SEGMENT
+response = endpoint.get_segment('LIST_UID', 'SEGMENT_UID')
+
+# DISPLAY RESPONSE
+puts response.body
+
+# GET ONE LIST SEGMENT SUBSCRIBERS
+response = endpoint.get_subscribers('LIST_UID', 'SEGMENT_UID', page = 1, per_page = 10)
 
 # DISPLAY RESPONSE
 puts response.body
--- mailwizz/endpoint/list_segments.rb
+++ mailwizz/endpoint/list_segments.rb
@@ -24,6 +24,44 @@ module Mailwizz
                             })
         client.request
       end
+
+      ##
+      # Get one list segment
+      #
+      # @param [String] list_uid
+      # @param [String] segment_uid
+      # @return [excon/Response]
+      ##
+      def get_segment(list_uid, segment_uid)
+        client = Client.new({
+                              'method': Client::METHOD_GET,
+                              'url': Base.config.api_url(sprintf('lists/%s/segments/%s', list_uid, segment_uid)),
+                              'params_get': {}
+                            })
+        client.request
+      end
+
+      ##
+      # Get subscribers from a certain mail list segment
+      #
+      # @param [String] list_uid
+      # @param [String] segment_uid
+      # @param [Integer] page
+      # @param [Integer] per_page
+      # @return [excon/Response]
+      ##
+      def get_subscribers(list_uid, segment_uid, page = 1, per_page = 10)
+
+        client = Client.new({
+                              'method': Client::METHOD_GET,
+                              'url': Base.config.api_url(sprintf('lists/%s/segments/%s/subscribers', list_uid, segment_uid)),
+                              'params_get': {
+                                'page': page,
+                                'per_page': per_page
+                              }
+                            })
+        client.request
+      end
     end
   end
 end
